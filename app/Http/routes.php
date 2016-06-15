a9a2e9a269f920450f2116079e791b6b4cdb5e03
qq459673705 <458440866@qq.com>
2016-06-15 18:45:35 +0800

    fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,26 +15,51 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::any('user', function(){  //这里除了get之外都不能用,原因是因为
-  return 'any';
+//Route::any('user', function(){  //这里除了get之外都不能用,原因是因为
+//  return 'any';
+//});
+//
+//Route::put('user', function(){
+//    return 'put';
+//});
+//
+//Route::patch('user', function(){
+//    return 'patch';
+//});
+//
+//Route::delete('user', function(){
+//    return 'delete';
+//});
+//
+//Route::post('user', function(){
+//    return 'post';
+//});
+//
+// Route::get('hd', function(){
+//   return 'get';
+// });
+//
+//Route::match(['get', 'post', 'patch'], 'hd', function(){
+//    return 'match';
+//});
+
+Route::get('test/{aa}', function($aa){
+    return 'test' . $aa;
 });
 
-Route::put('user', function(){
-    return 'put';
+
+Route::get('user/{name}/index/{password}', function($n, $p){  //测试用  http://localhost/blog/user/mingming   不要使用 http://localhost/blog/user?name=mingming 解析不了
+    return $n . "  " . $p;
 });
 
-Route::patch('user', function(){
-    return 'patch';
+Route::get('user_{name}', function($n){
+    return $n;
 });
 
-Route::delete('user', function(){
-    return 'delete';
-});
+//Route::get('foo/{name?}', function($name = '你好'){ // 参数加?之后 , 对应的闭包函数中 的参数需要有默认值
+//    return $name;
+//});
 
-Route::post('user', function(){
-    return 'post';
+Route::get('foo/{name?}/com/{pass?}', function($name = 'niho', $pass = '****'){ //多个参数除了最后一个参数之外,不能出现其他的可选参数否则容易出问题
+    return $name . "--------" . $pass;
 });
-
- Route::get('hd', function(){
-   return 'get';
- });
