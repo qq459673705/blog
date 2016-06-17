dd0530c144065b98f928f1c020c5017912185e8f
qq459673705 <458440866@qq.com>
2016-06-17 21:28:42 +0800

    add laravel-ide-helper

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -10,22 +10,28 @@ use \Illuminate\Support\Facades\DB;
 class IndexController extends Controller
 {
     //
-    public function index(){
-      echo config('database.connections.mysql.prefix'); //这里传入的是config目录下文件名，以及对应的key
-      return view('index/index');
+    public function index()
+    {
+        echo config('database.connections.mysql.prefix'); //这里传入的是config目录下文件名，以及对应的key
+        return view('index/index');
     }
 
-    public function content(){
-      return view('index/content');
+    public function content()
+    {
+        return view('index/content');
     }
 
-    public function layout(){
-      return view('index/layout');
+    public function layout()
+    {
+        return view('index/layout');
     }
 
-    public function getDB(){
-      $db = DB::connection()->getPdo();
-      dd($db);
-      return view('welcome');
+    public function getDB()
+    {
+//      $db = DB::connection()->getPdo();
+//      dd($db);
+        $user = DB::table('user')->get();
+        dd($db);
+        return view('welcome');
     }
 }
